0a9cff7f02e4fb64368d58b7954fda6aa65d56de
gambrivius <claponsie@gmail.com>
2023-02-14 13:14:16 -0500

    Added addition debugging info

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,10 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   console.log("TEST");
   console.log(`${request.method} ${request.nextUrl.pathname}`);
+  // get the host name
+  const host = request.headers.get("host");
+  console.log("host", host);
+  console.log("body", request.body);
+
   return NextResponse.next();
 }
