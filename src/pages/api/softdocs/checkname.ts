f8fe89b3c84f569b14bcb12e9964f78dfa84590a
gambrivius <claponsie@gmail.com>
2023-02-14 16:27:11 -0500

    Using conditional actor

--- src/pages/api/softdocs/checkname.ts
+++ src/pages/api/softdocs/checkname.ts
@@ -3,13 +3,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { namesDb } from "../../../fakedb";
 
 type ResponseData = {
-  approved: boolean;
+  actor: string;
 };
 
 // get and post request handler
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<string>
+  res: NextApiResponse<ResponseData>
 ) {
   console.log("checkname.ts: handler() called");
   console.log("body", req.body);
@@ -17,13 +17,13 @@ export default function handler(
     // validate body
     if (!req.body.name) {
       console.log("checkname.ts: handler() called with no name");
-      res.status(200).json("claponsie@kvcc.edu");
+      res.status(200).json({ actor: "claponsie@kvcc.edu" });
       return;
     }
 
     // check if name is in db
     const approved = namesDb.names.includes(req.body.name.toUpperCase());
     //res.status(200).json({ approved });
-    res.status(200).json("claponsie@kvcc.edu");
+    res.status(200).json({ actor: "claponsie@kvcc.edu" });
   }
 }
