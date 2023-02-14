2994b9ea79fd40a9bba808d1c1e13981cb1790da
gambrivius <claponsie@gmail.com>
2023-02-14 13:20:34 -0500

    more

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,13 +6,8 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   console.log("TEST");
   console.log(`${request.method} ${request.nextUrl.pathname}`);
-  // get the origin
-  const origin = request.headers.get("origin");
-  console.log("origin", origin);
 
-  // get the host name
-  const host = request.headers.get("host");
-  console.log("host", host);
+  console.log("header", request.headers);
   console.log("body", request.body);
 
   return NextResponse.next();
