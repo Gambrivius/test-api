884466b205522c0d1ff21777f2b1237d8a42af1e
gambrivius <claponsie@gmail.com>
2023-02-14 13:23:52 -0500

    more

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,11 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 // log all requests
-export function middleware(request: NextRequest) {
+export default async function asyncmiddleware(request: NextRequest) {
   console.log("TEST");
   console.log(`${request.method} ${request.nextUrl.pathname}`);
 
-  console.log("header", request.headers);
-  console.log("body", request.body);
+  console.log("request", request);
 
   return NextResponse.next();
 }
